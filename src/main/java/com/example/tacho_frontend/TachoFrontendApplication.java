6b5d7d5cb6dd26b25b1fd712947453b4c140b533
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2021-05-20 12:47:31 +0200

    first commit

---
+++ src/main/java/com/example/tacho_frontend/TachoFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacho_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachoFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TachoFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tacho_frontend/TachoFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacho_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TachoFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
